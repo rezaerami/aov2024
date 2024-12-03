0fec473a4274f4a7ea515150fefd0ec4dc6857ff
Reza Erami <reza.eramikalleh@hoganlovells.com>
2024-12-03 09:26:00 +0100

    add sum helper and use it in day 3

--- src/challenges/day-3.js
+++ src/challenges/day-3.js
@@ -1,22 +1,23 @@
 const path = require('path');
 const { readInput } = require('../utils/input.utils');
 const { timer } = require('../utils/timer.utils');
+const { sum } = require('../utils/math.utils');
 
 const inputFilePath = path.resolve('inputs', 'day-3.txt');
 
 const input = readInput(inputFilePath).join('');
 timer.start();
 
-const partOne = [...input.matchAll(/mul\((\d+),(\d+)\)/gm)].reduce(
-  (acc, curr) => acc + curr[1] * curr[2],
-  0,
+const pattern = /mul\((\d+),(\d+)\)/gm;
+const partOne = sum(
+  [...input.matchAll(pattern)].map((curr) => curr[1] * curr[2]),
 );
 
-const partTwo = [
-  ...input
-    .replace(/don't\(\)(.*?)(do\(\)|$|\n)/gm, '')
-    .matchAll(/mul\((\d+),(\d+)\)/gm),
-].reduce((acc, curr) => acc + curr[1] * curr[2], 0);
+const partTwo = sum(
+  [...input.replace(/don't\(\)(.*?)(do\(\)|$|\n)/gm, '').matchAll(pattern)].map(
+    (curr) => curr[1] * curr[2],
+  ),
+);
 
 console.table({
   'Part 1': partOne,
---
+++ src/utils/math.utils.js
@@ -0,0 +1,5 @@
+const sum = (array) => array.reduce((a, b) => a + b, 0);
+
+module.exports = {
+  sum
+}

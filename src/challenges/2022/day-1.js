31947eae595371009364a37408c74b1261aeb1a1
Reza Erami <reza.eramikalleh@hoganlovells.com>
2025-01-27 07:04:45 +0100

    add generic parser for parsing with callback

--- src/challenges/2022/day-1.js
+++ src/challenges/2022/day-1.js
@@ -1,17 +1,16 @@
 const path = require('path');
-const { readInput } = require('../..//utils/input.utils');
+const { genericParser } = require('../..//utils/input.utils');
 const { timer } = require('../..//utils/timer.utils');
 const { sum } = require('../../utils/math.utils');
 
 const inputFilePath = path.resolve('inputs', '2022', 'day-1.txt');
 
-const input = readInput(inputFilePath, false);
+const input = genericParser(inputFilePath, '\n\n', (chunk) =>
+  sum(chunk.split('\n').map(Number)),
+);
 timer.start();
 
-const elvesStorage = input
-  .split('\n\n')
-  .map((item) => sum(item.split('\n').map(Number)))
-  .sort((a, b) => (a < b ? 1 : -1));
+const elvesStorage = input.sort((a, b) => (a < b ? 1 : -1));
 
 const partOne = elvesStorage[0];
 const partTwo = sum(elvesStorage.slice(0, 3));
--- src/utils/input.utils.js
+++ src/utils/input.utils.js
@@ -1,13 +1,18 @@
 const fs = require('fs');
 
-const readInput = (inputFilePath, parse = true) => {
+const readInput = (inputFilePath, parse = true, splitter = '\n') => {
   const fileContent = fs.readFileSync(inputFilePath, 'utf-8');
-  if (parse) return parseInput(fileContent);
+  if (parse) return parseInput(fileContent, splitter);
 
   return fileContent;
 };
+const genericParser = (inputFilePath, splitter, parserCallback) => {
+  const chunks = readInput(inputFilePath, true, splitter);
+  return chunks.map((chunk) => parserCallback(chunk));
+};
 
-const parseInput = (input) => input.trim().split('\n');
+const parseInput = (input, splitter = '\n') => input.trim().split(splitter);
+
 const parseInputToCells = (input) =>
   readInput(input).map((line) => line.trim().split(''));
 
@@ -15,4 +20,5 @@ module.exports = {
   readInput,
   parseInput,
   parseInputToCells,
+  genericParser,
 };
